65f9d685822b296bb75525d9beaee347d33a29f5
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-05-03 23:17:37 +0200

    toolbar prototype

--- src/Editor.svelte
+++ src/Editor.svelte
@@ -3,6 +3,7 @@
   import { debounce } from "throttle-debounce";
 
   import Paragraph from "./Paragraph.svelte";
+  import Toolbar from "./Toolbar.svelte";
 
   const dispatch = createEventDispatcher();
 
@@ -13,7 +14,6 @@
     dispatch("init", {});
   };
   const onChange = () => {
-    console.log("onchange");
     dispatch("change", {});
   };
 
@@ -32,16 +32,17 @@
   export const content = {
     paragraphs: [
       {
-        text: "das ist ein test"
+        text:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. In ducimus sequi magni, quasi expedita debitis, ea tenetur quae vero illum placeat ullam omnis unde saepe. Nisi quo excepturi laudantium inventore."
       },
       {
-        text: "Lorem Ipsum dolor!"
+        text:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. In ducimus sequi magni, quasi expedita debitis, ea tenetur quae vero illum placeat ullam omnis unde saepe. Nisi quo excepturi laudantium inventore."
       }
     ]
   };
 
   onMount(() => {
-    console.log("huhu");
     onInit();
   });
 </script>
@@ -49,11 +50,21 @@
 <style>
   .omnia-editor {
     width: -webkit-fill-available;
+    margin-left: auto;
+    margin-right: auto;
+    font-family: Lora, sans-serif;
+    font-size: 21px;
+    line-height: 1.58;
+  }
+  #omnia-selection-ref {
+    position: absolute;
   }
 </style>
 
-<div class="omnia-editor">
+<div id="omnia-editor" class="omnia-editor">
+  <Toolbar />
   {#each content.paragraphs as paragraph}
     <Paragraph {...paragraph} on:change={debounce(500, onChange)} />
   {/each}
 </div>
+<div id="omnia-selection-ref" />
