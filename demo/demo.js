8fa0e0a7486a3ab024ceeab1e4fc5765c5eac126
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-07-08 00:36:00 +0200

    fix demo page

--- demo/demo.js
+++ demo/demo.js
@@ -1,10 +1,11 @@
 import Editor from "./index.mjs";
 import Data from "./data.js"
+console.log(Data)
 
 const editor = new Editor({
     target: document.getElementById("editor"),
     props: {
-        content: Data,
+        data: Data,
         active: true,
         toolbar: true
     }
