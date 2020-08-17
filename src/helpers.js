5f27655c722847f8c7803f2113ff61e6a3e3d0cf
Torsten Dittmann <t.dittmann@eslgaming.com>
2020-08-17 13:03:52 +0200

    add rangy for formatting

--- src/helpers.js
+++ src/helpers.js
@@ -1,13 +1,35 @@
+import rangy from "rangy/lib/rangy-core.js";
+import "rangy/lib/rangy-selectionsaverestore";
+import "rangy/lib/rangy-textrange";
+
 export const wrapSelection = tag => {
     if (!window.getSelection) return false;
-    const ele = document.createElement(tag);
-    const sel = window.getSelection();
+
+    const el = document.createElement(tag);
+    const sel = rangy.getSelection();
     if (!sel.rangeCount) return false;
-    if (sel.isCollapsed) {
-        console.log(ele)
-    }
+    
+    sel.expand("word");
+    
+    const savedSel = rangy.saveSelection();
+    const parentTag = findUpTag(sel.focusNode, tag);
     const range = sel.getRangeAt(0).cloneRange();
-    range.surroundContents(ele);
-    sel.removeAllRanges();
-    sel.addRange(range);
+    if (parentTag) {
+        parentTag.replaceWith(...parentTag.childNodes);
+    } else {
+        range.surroundContents(el);
+    }    
+    rangy.restoreSelection(savedSel)
+}
+
+
+const findUpTag = (el, tag) => {
+    while (el.parentNode) {
+        if (el.tagName.toLowerCase() === "div") 
+            return false;
+        if (el.tagName.toLowerCase() === tag)
+            return el;
+        el = el.parentNode;
+    }
+    return false;
 }
